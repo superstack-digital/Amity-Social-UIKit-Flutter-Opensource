c664cf862d3773eeba55a23f982d9519e564d5fc
kirk <kirk@nustechnology.com>
2026-05-04 11:42:56 +0700

    fix: update imports to use lowercase shimmer.dart

--- lib/v4/social/post/post_detail/amity_post_detail_page.dart
+++ lib/v4/social/post/post_detail/amity_post_detail_page.dart
@@ -8,7 +8,7 @@ import 'package:amity_uikit_beta_service/v4/social/post/amity_post_content_compo
 import 'package:amity_uikit_beta_service/v4/social/post/common/post_action.dart';
 import 'package:amity_uikit_beta_service/v4/social/post/post_detail/bloc/post_detail_bloc.dart';
 import 'package:amity_uikit_beta_service/v4/social/post/post_item/bloc/post_item_bloc.dart';
-import 'package:amity_uikit_beta_service/v4/utils/Shimmer.dart';
+import 'package:amity_uikit_beta_service/v4/utils/shimmer.dart';
 import 'package:amity_uikit_beta_service/v4/utils/skeleton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
